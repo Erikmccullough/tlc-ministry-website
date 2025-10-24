a683ef8481f4d7eafb7e549eff795e9cafab7b3a
Erikmccullough <erikmccullough73@gmail.com>
2025-10-24 12:28:58 -0700

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,4 +1,1 @@
-
-// Placeholder for potential JavaScript functionality
-// You can add interactive elements here later if needed.
-console.log('TLC Ministry site loaded');
+// JavaScript placeholder
